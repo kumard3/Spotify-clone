76d5ee51b79c6a768863dccaec5d629b11bbba15
Kumar Deepanshu <62591080+kumard3@users.noreply.github.com>
2020-10-23 01:23:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import Login from './Login'
+import './App.css';
+import { getTokenFromUrl } from './spotify';
+
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player.js';
+import { useDataLayerValue} from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
+function App() { 
+  const [{user , token }, dispatch ] = useDataLayerValue(); 
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash ="";
+    const _token = hash.access_token;
+    
+    if ( _token){
+      // setToken( _token);
+      spotify.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      spotify.getMe().then(user => {
+        
+        dispatch({
+           type: "SET_USER",
+           user: user ,
+        });
+      });
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists : playlists,
+        });
+      });
+
+    }
+  }, []);
+  return (
+    <div className="app"> { token ? <Player/>: <Login /> }
+     
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { DataLayer }from './DataLayer.js';
+import reducer , {initialState} from './reducer.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+serviceWorker.unregister();
